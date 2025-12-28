555fdf424e1c7703a0f11703e68d162cbbc9c9d0
SinaBimesl <sinabimesl2000@gmail.com>
2025-12-28 09:00:40 +0000

    fix api for run in server

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,6 +5,16 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '0.0.0.0',
-        port: 5173
+        port: 5173,
+        proxy: {
+            '/auth': {
+                target: 'http://localhost:8000',
+                changeOrigin: true
+            },
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true
+            }
+        }
     }
 })
