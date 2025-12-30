33a11dd4db3951cfa55900f58e1e7a4b983a34e1
SinaBimesl <sinabimesl2000@gmail.com>
2025-12-30 22:25:52 +0330

    Delete the old dir of front created By sina

--- frontend-react-OLD/src/store/hooks.js
+++
@@ -1,9 +0,0 @@
-/* ==========================================
-   NGCORION - Redux Hooks
-   ========================================== */
-
-import { useDispatch, useSelector } from 'react-redux';
-
-// Typed hooks for better development experience
-export const useAppDispatch = () => useDispatch();
-export const useAppSelector = useSelector;
