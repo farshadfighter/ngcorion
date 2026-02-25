6afcdf45cbdd2d96048b56bc3cc9d4b6c0041009
SinaBimesl <sinabimesl2000@gmail.com>
2026-02-25 20:26:30 +0330

    Alembic upgrade

---
+++ alembic/versions/add_mssql_to_devicetype_enum.py
@@ -0,0 +1,32 @@
+"""add_mssql_to_devicetype_enum
+
+Revision ID: c3d4e5f6a7b8
+Revises: b2c3d4e5f6a7
+Create Date: 2026-02-25 00:00:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'c3d4e5f6a7b8'
+down_revision: Union[str, Sequence[str], None] = 'b2c3d4e5f6a7'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Add 'mssql' value to the devicetype PostgreSQL enum."""
+    # ALTER TYPE ... ADD VALUE cannot run inside a transaction in PostgreSQL
+    with op.get_context().autocommit_block():
+        op.execute("ALTER TYPE devicetype ADD VALUE IF NOT EXISTS 'mssql'")
+
+
+def downgrade() -> None:
+    """
+    PostgreSQL does not support removing enum values directly.
+    A full enum recreation would be required; left as no-op for safety.
+    """
+    pass
