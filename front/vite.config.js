49ef5a113798e789bd315c763bbf3592d2931f97
SinaBimesl <sinabimesl2000@gmail.com>
2026-05-04 09:30:50 -0400

    change port to 80

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        proxy: {
-            '/auth': {
-                target: 'http://172.16.200.90:8000',
-                changeOrigin: true
-            },
-            '/api': {
-                target: 'http://172.16.200.90:8000',
-                changeOrigin: true
-            }
-        }
-    }
-})
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 80,
+    strictPort: true,
+    proxy: {
+      "/auth": {
+        target: "http://172.16.200.90:8000",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://172.16.200.90:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
