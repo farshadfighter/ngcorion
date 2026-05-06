c33a731592cf744d4b071f25571c1b3b52390971
SinaBimesl <sinabimesl2000@gmail.com>
2026-05-06 04:19:20 -0400

    fix vendor Type and network zone description

--- app/modules/assets/schemas.py
+++ app/modules/assets/schemas.py
@@ -262,6 +262,7 @@ class AssetLocationResponse(AssetLocationCreate):
 # Network Zones
 class NetworkZoneCreate(BaseModel):
     zone_name: str
+    description: Optional[str] = None
 
 class NetworkZoneResponse(NetworkZoneCreate):
     id: int
@@ -280,11 +281,15 @@ class OSCatalogResponse(OSCatalogCreate):
 # Vendor Catalog
 class VendorCatalogCreate(BaseModel):
     vendor_name: str
+    vendor_type: Optional[str] = None
+    #website: Optional[str] = None
+    #description: Optional[str] = None
 
 class VendorCatalogResponse(VendorCatalogCreate):
     id: int
 
     model_config = ConfigDict(from_attributes=True)
+    
 
 # Asset Dependencies
 class AssetDependencyCreate(BaseModel):
