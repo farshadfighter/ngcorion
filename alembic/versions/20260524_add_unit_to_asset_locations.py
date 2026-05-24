22e4d5ca3f5c2f2382ddcbb5d1ff4e82a150dc67
SinaBimesl <sinabimesl2000@gmail.com>
2026-05-24 11:52:14 +0330

    add unit

--- alembic/versions/20260524_add_unit_to_asset_locations.py
+++ alembic/versions/20260524_add_unit_to_asset_locations.py
@@ -1,16 +1,16 @@
 """add unit to asset_locations
 
-Revision ID: a1b2c3d4e5f6
-Revises: e02d17baefa3
-Create Date: 2026-05-23
+Revision ID: 20260524_add_unit
+Revises: 20260523_drop_loc_net
+Create Date: 2026-05-24
 
 """
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = 'e02d17baefa3'
+revision: str = '20260524_add_unit'
+down_revision: Union[str, Sequence[str], None] = '20260523_drop_loc_net'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
